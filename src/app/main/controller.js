17696fb62012d8c874e469cfe1d24c3762ac07d7
Nathan Sass <nsass711@gmail.com>
2015-08-21 10:49:56 -0700

    Have api calls to weather and location service

--- src/app/main/controller.js
+++ src/app/main/controller.js
@@ -6,10 +6,59 @@ angular
     var myId = 'OICKGWOXVDITIP00YRSWBKZ2JCBE0EIU1KI3GJDMZE1LYZ3O';
     var mySecret = 'GJ3CMLRXSKTJH1QS5YBADM422IJG5IIJP0Y55DH2ATBDHTPM';
 
-    $http.get('https://api.foursquare.com/v2/venues/explore?client_id=' + myId + '&client_secret=' + mySecret + '&v=20130815&ll=37.794,-122.408&query=park').success(function(data) {
-      $scope.explore = data;
-    });
+    var lat = '37.794';
+    var longi = '-122.408';
+    // var latLong = '37.794,-122.408';
+    var offset = Math.floor((Math.random() * 20) + 1 );
+    
+    var locationRequest = 'https://api.foursquare.com/v2/venues/explore?client_id=' +
+                 myId +
+                 '&client_secret=' +
+                 mySecret +
+                 '&v=20130815&' +
+                 'll=' + lat + ',' + longi +
+                 '&query=mountain' +
+                 // '&query=sushi' +
+                 '&offset=' + offset +
+                 // '&limit=10' +
+                 '&radius=130000' +
+                 '&sortByDistance=1';
 
+    // var forecastKey = '4233057dd5ff3c197d4ae39ae8b05582';
+    var weatherRequest = 'http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + longi;
+
+    
+    var apiCall = function (url, callback) {
+        $http.get(url).success(function(data) {
+          callback(data);
+        });
+    };
+
+    var randNum = function (max) {
+      return Math.floor((Math.random() * max) + 1 );
+    };
+
+    var locationCallback = function (data){
+      $scope.currentLocation = data.response.headerFullLocation;
+      $scope.places = data.response.groups[0].items; //need venue.name 
+    };
+
+    var weatherCallback = function (data) {
+      $scope.currentCondition = data.weather[0]['description'];
+      $scope.currentTemp = data.main.temp;
+      console.log(data);
+    };
+
+    // $http.get(weatherRequest).success(function(data) {
+    //       console.log(data);
+    // });
+
+    apiCall(locationRequest, locationCallback);
+    // console.log(weatherRequest);
+    apiCall(weatherRequest, weatherCallback);
+
+    // $http.get(locationRequest).success(function(data) {
+    // });
 
 
 
