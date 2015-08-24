2629b8994e666f7d69171a27c4209a2a668cf8f1
Nathan Sass <nsass711@gmail.com>
2015-08-24 16:44:34 -0700

    Redoing design pattern because of issues with $scope

--- src/app/main/controller.js
+++ src/app/main/controller.js
@@ -1,87 +1,75 @@
+'use strict'
+
 angular
   .module('adventureplanner.main')
-  .controller('MainCtrl', function ($scope, $window, $http) {
-    'use strict';
-        
-    var randNum = function (max) {
-      return Math.floor((Math.random() * max) + 1 );
+  .factory('dataService', function dataService (locationService){
+    var instance = {
+      getVenues: getVenues,
+      venues: ''
+    };
+
+    return instance;
+
+    //////////////
+
+    function getVenues () {
+      return locationService.get()
+        .then(function (response) {
+          instance.venues = response.data.response.groups[0].items;
+          return {venues: instance.venues};
+          // console.log(response.data.response);
+        })
+    }
+  })
+  .factory('locationService', function locationService ($http) {
+    
+    var service = {
+      get: get,
+      testHttp: function(){ console.log('http Service Working');}
     };
     
-    var locationRequestParams = {
-      id: 'OICKGWOXVDITIP00YRSWBKZ2JCBE0EIU1KI3GJDMZE1LYZ3O',
-      secret: 'GJ3CMLRXSKTJH1QS5YBADM422IJG5IIJP0Y55DH2ATBDHTPM',
-      lat: '37.794',
-      lng: '-122.408',
-      offset: randNum(20),
-      query: 'mountain'
-    };
+    return service;
     
-    var buildLocationRequest = function (request) {
+    //////////////
+    
+
+    function get() {
+      var venueUrl = _buildLocationRequest (locationParams());
+      return $http.get(venueUrl);
+    }
+
+    function _buildLocationRequest (params) {
       return 'https://api.foursquare.com/v2/venues/explore?client_id=' +
-                 request.id +
+                 params.id +
                  '&client_secret=' +
-                 request.secret +
+                 params.secret +
                  '&v=20130815&' +
-                 'll=' + request.lat + ',' + request.lng +
-                 '&query=' + request.query +
-                 '&offset=' + request.offset +
+                 'll=' + params.lat + ',' + params.lng +
+                 '&query=' + params.query +
+                 '&offset=' + params.offset +
                  '&radius=130000' +
                  '&sortByDistance=1';
-    };
+    }
 
-    var buildWeatherRequest = function(lat, lng) {
-      return 'http://api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lng;
-    };
-    
-    var apiCall = function (url, callback, index) {
-        $http.get(url).success(function(data) {
-          callback(data, index);
-        });
-    };
-
-    var iterateOverLocations = function (places) {
-      var lat, lng;
-      places.forEach( function (place, i) {
-        $scope.i = i;
-        lat = place.venue.location.lat;
-        lng = place.venue.location.lng;
-        var wRequest = buildWeatherRequest (lat, lng);
-        apiCall(wRequest, weatherCallback, i);
-      });
-    };
-
-    var locationCallback = function (data){
-      $scope.currentLocation = data.response.headerFullLocation;
-      $scope.places = data.response.groups[0].items; //need venue.name
-      iterateOverLocations ($scope.places);
-    };
-
-
-    var weatherCallback = function (data, i) {
-      var locationCondition = data.weather[0]['description'];
-      var locationTemp = data.main.temp;
-      if (i >= 0) {
-        $scope.places[i].condition = locationCondition;
-        $scope.places[i].temp = locationTemp;
-      } else {
-        $scope.currentCondition = locationCondition;
-        $scope.currentTemp = locationTemp;
+    function locationParams (params ) {
+      if (!params) {params = {};}
+      var max = params.hasOwnProperty('max') ? params.max : 20;
+      var queryVenue = params.hasOwnProperty('query') ? params.query : 'mountain';
+      
+      function _randNum () {
+        return Math.floor((Math.random() * max) + 1 );
       }
-
-    };
-
-    $scope.triggerAdventureIdeas = function(flag) {
-      if (flag !== 'init') {
-        locationRequestParams.query = $scope.userSearchTerm;
-      }
-      var lRequest = buildLocationRequest(locationRequestParams);
-      console.log(lRequest);
-      apiCall(lRequest, locationCallback);
-    };
-
-    var wRequest = buildWeatherRequest(locationRequestParams.lat, locationRequestParams.lng);
-    apiCall(wRequest, weatherCallback);
-
-    $scope.triggerAdventureIdeas('init');
-
+      return {
+        id: 'OICKGWOXVDITIP00YRSWBKZ2JCBE0EIU1KI3GJDMZE1LYZ3O',
+        secret: 'GJ3CMLRXSKTJH1QS5YBADM422IJG5IIJP0Y55DH2ATBDHTPM',
+        lat: '37.794',
+        lng: '-122.408',
+        offset: _randNum(20),
+        query: queryVenue
+      };
+    }
+  })
+  .controller('MainCtrl', function ($scope, $window, dataService) {
+    $scope.service = dataService;
+    $scope.service.getVenues();
   });
